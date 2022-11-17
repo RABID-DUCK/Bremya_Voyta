1b7db8fcd1e6fabca838e61afd53b864207e99f0
Vezyang <78100632+Vezyang@users.noreply.github.com>
2022-11-18 02:04:31 +0500

    Added new player prefab, change hierarchy

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,43 +4,40 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float vertical, horizontal;
-    //private float mouseX, mouseY;
-    //private float minAngle, maxAngle;
 
     [Header("Скорость перемещения персонажа")]
-    [SerializeField] private float speed = 7f;
+    [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float turningSpeed = 7f;
 
-    [SerializeField] private PhotonView view;
+    [SerializeField] private PhotonView photonView;
 
     [SerializeField] private float sensitivityMouse;
 
-    [SerializeField] private GameObject camera;
-    //[SerializeField] private Transform cameraTarget;
-
-    [SerializeField] private PlayerController scriptPlayerController;
-    [SerializeField] private Transform player;
+    [SerializeField] private Transform playerBody;
 
     private void Awake()
     {
-        view = GetComponent<PhotonView>();
-        if (!view.IsMine)
+        if (!photonView)
         {
-            camera.SetActive(false);
-            scriptPlayerController.enabled = false;
+            photonView = GetComponent<PhotonView>();
+        }
+        if (!photonView.IsMine)
+        {
+            enabled = false;
         }
     }
+
     private void Update()
     {
-        PlayerMov();
+        PlayerMove();
     }
-    void PlayerMov()
+
+    private void PlayerMove()
     {
         horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
-        vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        vertical = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
-        player.transform.Rotate(0, horizontal, 0);
+        playerBody.transform.Rotate(0, horizontal, 0);
         transform.Translate(0, 0, vertical);
-
     }
 }
