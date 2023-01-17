1828cbf5cd5297584004f87a1add31e874291224
DESKTOP-9IEC2AK\katam <kata.mayorrr@gmail.com>
2023-01-18 00:50:57 +0500

    Scripts for Sleep system

--- Assets/Scripts/Examples/Bed.cs
+++ Assets/Scripts/Examples/Bed.cs
@@ -9,17 +9,35 @@ public class Bed : MonoBehaviour
 
     [SerializeField] private LayerMask sleepLayerMask;
     [SerializeField] private GameObject sleepWindow;
-    [SerializeField] private GameObject Player;
+    [SerializeField] private GameObject player;
     [SerializeField] private GameObject playerSleep;
 
-    [SerializeField] private bool dayTime;
-    void Start()
+    [SerializeField] private WarningSleepPanel warningSleepPanel;
+    private bool IsSleep { get; set; }
+
+    private void OnEnable()
+    {
+        worldTime = FindObjectOfType<WorldTime>();
+    }
+
+
+    private void Start()
     {
         outline = transform.gameObject.GetComponent<Outline>();
+
+        sleepPanel.OnSleep += Sleep;
+
+        //warningSleepPanel.OnDontSleep += ForcedSleep;
     }
+
     private void OnMouseOver()
     {
         outline.enabled = true;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            StateSleep();
+        }
     }
 
     private void OnMouseExit()
@@ -27,38 +45,94 @@ public class Bed : MonoBehaviour
         outline.enabled = false;
     }
 
-    void Update()
+    private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (worldTime.CheckTimeOfDay)
         {
-            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, sleepLayerMask))
-            {
-                StateSleep();
-            }
+            Instantiate(player, new Vector3(0.2f, 0.08f, 0.7f), Quaternion.LookRotation(new Vector3(0, 0, 0)));
+
+            IsSleep = false;
+        }
+        if (!worldTime.CheckTimeOfDay)
+        {
+            warningSleepPanel.ShowWarningSleepPanel(worldTime.CheckTimeOfDay);
         }
     }
-    //private void OnMouseDown()
-    //{
-    //    StateSleep();
-    //}
-    public void StateSleep()
-    {
-        //sleepWindow.SetActive(true);
 
+    private void StateSleep()
+    {
         if (!worldTime.CheckTimeOfDay)
         {
             print("night");
             sleepWindow.SetActive(true);
-            sleepPanel.ShowSleepPanel();
         }
         else
         {
             print("day");
-            print(worldTime.timeProgress);
-            playerSleep.SetActive(false);
-            Player.SetActive(true);
+            print(worldTime.CheckTimeOfDay);
         }
     }
+
+    private void Sleep()
+    {
+        Instantiate(player, new Vector3(-0.984f, 0.4f, -0.261f), Quaternion.LookRotation(new Vector3(0, 90f, 0)));
+
+        IsSleep = true;
+    }
 }
+
+    
+    
+    
+    
+    //
+    //void Start()
+    //{
+    //    outline = transform.gameObject.GetComponent<Outline>();
+    //}
+    //
+    //private void OnMouseOver()
+    //{
+    //    outline.enabled = true;
+    //}
+    //
+    //private void OnMouseExit()
+    //{
+    //    outline.enabled = false;
+    //}
+    //
+    //void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+    //        RaycastHit hit;
+    //        if (Physics.Raycast(ray, out hit, sleepLayerMask))
+    //        {
+    //            StateSleep();
+    //        }
+    //    }
+    //}
+    ////private void OnMouseDown()
+    ////{
+    ////    StateSleep();
+    ////}
+    //public void StateSleep()
+    //{
+    //    //sleepWindow.SetActive(true);
+    //
+    //    if (!worldTime.CheckTimeOfDay)
+    //    {
+    //        print("night");
+    //        sleepWindow.SetActive(true);
+    //        sleepPanel.ShowSleepPanel();
+    //    }
+    //    else
+    //    {
+    //        print("day");
+    //        print(worldTime.timeProgress);
+    //        playerSleep.SetActive(false);
+    //        Player.SetActive(true);
+    //    }
+    //}
+
--- Assets/Scripts/Player/SleepPanel.cs
+++ Assets/Scripts/Player/SleepPanel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,64 +6,28 @@ using UnityEngine.UI;
 
 public class SleepPanel : MonoBehaviour
 {
-    private bool yesBtn;
-    private bool noBtn;
-
-    PlayerController playerController;
-
-    [SerializeField] private Button warningYes;
-
     [SerializeField] private GameObject player;
-    [SerializeField] private GameObject playerSleep;
-    [SerializeField] private GameObject sleepWindow;
-    //[SerializeField] private GameObject warningWindow;
 
-    float notSleep=0;
-    public void BtnYes()
+    [SerializeField] private GameObject sleepPanel;
+
+    [SerializeField] private Button yesButton;
+    [SerializeField] private Button noButton;
+
+    public event Action OnSleep;
+
+    private void Start()
     {
-        yesBtn = true;
-        ShowSleepPanel();
+        yesButton.onClick.AddListener(SleepSender);
+        noButton.onClick.AddListener(ClosePanel);
     }
-    public void BtnNo()
+
+    private void ClosePanel()
     {
-        noBtn = true;
-        ShowSleepPanel();
+        sleepPanel.SetActive(false);
     }
-    public void ShowSleepPanel()
+
+    private void SleepSender()
     {
-        if (yesBtn==true)
-        {
-            sleepWindow.SetActive(false);
-            playerSleep.SetActive(true);
-            player.SetActive(false);
-        }
-        else if (noBtn == true)
-        {
-            if (notSleep == 0)
-            {
-                sleepWindow.SetActive(false);
-                notSleep += 1;
-            }
-            else if (notSleep >= 1)
-            {
-                sleepWindow.SetActive(false);
-                //warningWindow.SetActive(true);
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            playerSleep.SetActive(false);
-            player.SetActive(true);
-        }
-    }
-    public void WarningBtnYes()
-    {
-        //warningWindow.SetActive(false);
-        playerSleep.SetActive(true);
-        player.SetActive(false);
-    }
-    public void WarningBtnNo()
-    {
-        print("тут он будет валиться на камнях");
+        OnSleep?.Invoke();
     }
 }
