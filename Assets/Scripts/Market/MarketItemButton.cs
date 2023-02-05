3a373e9ea5f5c82185747356e0c57abd7ad74a3b
Vezyang <78100632+Vezyang@users.noreply.github.com>
2023-02-05 23:59:21 +0500

    Added full market panel

--- Assets/Scripts/Market/MarketItemButton.cs
+++ Assets/Scripts/Market/MarketItemButton.cs
@@ -1,45 +1,8 @@
-using System;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class MarketItemButton : MonoBehaviour
+public class MarketItemButton : ShopItemButton
 {
-    public Action<Item> OnClick;
-
-    [SerializeField] private Image itemIcon;
-    [SerializeField] private Button buyButton;
-    [SerializeField] private TextMeshProUGUI priceText;
-
-    private Item buttonItem = null;
-
-    private void Awake()
-    {
-        buyButton.onClick.AddListener(OnClickByButton);
-    }
-
-    public void Initialization(SellItem sellItem)
-    {
-        buttonItem = sellItem.item;
-        Sprite itemSprite = buttonItem.ItemSprite;
-
-        if (itemSprite)
-        {
-            itemIcon.sprite = itemSprite;
-        }
-        else
-        {
-            itemIcon.gameObject.SetActive(false);
-        }
-
-        priceText.text = sellItem.price.ToString();
-    }
-
-    private void OnClickByButton()
-    {
-        if (buttonItem)
-        {
-            OnClick?.Invoke(buttonItem);
-        }
-    }
+    [Space]
+    [SerializeField] private TextMeshProUGUI playerNmaeText;
 }
