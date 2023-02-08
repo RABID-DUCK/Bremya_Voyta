9673e7bd4804bb22e42452b4278b63b76d20660e
FilinGoy <den_suvorov2003@mail.ru>
2023-02-09 00:55:22 +0500

    AddTeleportInPlayer
    Метод телепорта в Character.

--- Assets/Scripts/Events/WaterTrigger.cs
+++ Assets/Scripts/Events/WaterTrigger.cs
@@ -13,14 +13,11 @@ public class WaterTrigger : MonoBehaviourPunCallbacks
     {
         if (other.gameObject.GetPhotonView().IsMine)
         {
-            CharacterController _controller = other.GetComponent<CharacterController>();
+            Character character = other.GetComponent<Character>();
 
             Transform posHouse = houses.FirstOrDefault(h => other.gameObject.GetPhotonView().Owner.NickName == h.playerNick).doorway;
 
-            _controller.enabled = false;
-            other.transform.position = other.transform.TransformVector(posHouse.position);
-            other.transform.GetChild(0).rotation = posHouse.transform.rotation;
-            _controller.enabled = true;
+            character.Teleport(posHouse.position, posHouse.transform.rotation);
         }
     }
 }
