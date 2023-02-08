78f3c7c059ea5bec4d1492671fe3f643eba5595d
oLitva <101389645+oLitva@users.noreply.github.com>
2023-02-08 21:51:06 +0500

    Add sound in game

--- Assets/Scripts/Multiplayer/SpawnManager.cs
+++ Assets/Scripts/Multiplayer/SpawnManager.cs
@@ -28,6 +28,8 @@ public class SpawnManager : MonoBehaviourPunCallbacks
         string nameCharacter = _character.prefabs[_idCh].name;
         GameObject _locPlayer = PhotonNetwork.Instantiate(Path.Combine($"PhotonPrefabs/{_character.name}", $"{nameCharacter}"), new Vector3(0, 7, 0), Quaternion.identity);
 
+        _locPlayer.AddComponent<AudioListener>();
+       
         _camera.Follow = _locPlayer.transform;
         sceneCoordinator.InitializationPlayer(_locPlayer.GetComponent<Character>());
         _locPlayer.GetComponent<Character>().PlayerWallet.PutCoins(50);
