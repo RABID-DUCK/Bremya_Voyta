cf98e669a9156bf1a6b68a1fd29d3384641a4c30
FilinGoy <den_suvorov2003@mail.ru>
2023-03-20 17:26:33 +0500

    PofigGlavnoeChtoFix

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using TMPro;
-using UnityEditor.XR;
 using UnityEngine;
 using UnityEngine.UI;
 using Hastable = ExitGames.Client.Photon.Hashtable;
